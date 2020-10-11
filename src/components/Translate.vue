952dbe19f32deed03e2c4702f38c5462c14d2600
YuikoIto <toffywing@gmail.com>
2020-10-11 22:12:54 +0900

    レイアウト調整

--- src/components/Translate.vue
+++ src/components/Translate.vue
@@ -4,53 +4,64 @@
       <v-file-input
         v-model="file"
         @change="setImage"
-        @click:clear="clearCanvas"
+        @click:clear="clearData"
         accept="image/*"
         prepend-icon="mdi-camera"
         placeholder="画像をドラッグ＆ドロップか選択してください。"
       ></v-file-input>
-      <v-btn
-        class="my-2"
-        color="primary"
-        :disabled="!file"
-        v-show="file"
-        block
-        @click="getCloudVisionResult(file)"
-        >文字起こしする</v-btn
-      >
-      <v-btn
-        class="my-2"
-        color="primary"
-        :disabled="!file"
-        block
-        v-show="results"
-        @click="getTranslateResult(results)"
-        >翻訳する</v-btn
-      >
-      <v-btn
-        class="my-2"
-        color="primary"
-        :disabled="!file"
-        block
-        v-show="!results && file"
-        @click="getTranslateResultNoDocs(file)"
-        >翻訳する</v-btn
-      >
-      <v-row justify="center" align="center">
-        <canvas width="windowWidth" ref="preview"></canvas>
+      <v-row>
+        <v-col>
+          <img width="100%" v-if="uploadImageUrl" :src="uploadImageUrl" />
+        </v-col>
+        <v-col>
+          <v-btn
+            block
+            color="primary"
+            :disabled="!file"
+            v-show="file && !results"
+            @click="getCloudVisionResult(file)"
+            >文字起こしする</v-btn
+          ><v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon v-on="on" @click="doCopyText" v-show="file && results">
+                mdi-checkbox-multiple-blank-outline
+              </v-icon>
+            </template>
+            コピーする
+          </v-tooltip>
+          <v-input type="text" v-show="file" v-model="textMessage" readonly>
+            {{ this.results }}</v-input
+          >
+          <v-btn
+            block
+            color="primary"
+            :disabled="!file"
+            v-show="file && !translateResult"
+            @click="getTranslateResult(results | file)"
+            >翻訳する</v-btn
+          >
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon
+                v-on="on"
+                @click="doCopyTranslateText"
+                v-show="file && translateResult"
+              >
+                mdi-checkbox-multiple-blank-outline
+              </v-icon>
+            </template>
+            コピーする
+          </v-tooltip>
+          <v-input
+            v-show="file"
+            type="text"
+            v-model="translateTextMessage"
+            readonly
+          >
+            {{ this.translateResult }}</v-input
+          >
+        </v-col>
       </v-row>
-      <v-input type="text" v-model="textMessage" readonly
-        >{{ this.results
-        }}<v-btn type="button" @click="doCopyText">
-          コピーする
-        </v-btn></v-input
-      >
-      <v-input type="text" v-model="translateTextMessage" readonly
-        >{{ this.translateResult
-        }}<v-btn type="button" @click="doCopyTranslateText">
-          コピーする
-        </v-btn></v-input
-      >
     </v-container>
   </v-app>
 </template>
@@ -66,9 +77,9 @@ export default {
       translateResult: '',
       file: null,
       lang: 'ja',
-      windowWidth: window.parent.screen.width,
       textMessage: '',
       translateTextMessage: '',
+      uploadImageUrl: '',
     };
   },
   methods: {
@@ -83,42 +94,27 @@ export default {
 
     addDropFile(e) {
       if (this.file) {
-        this.clearCanvas();
+        this.clearData();
         this.file = e.dataTransfer.files[0];
-        this.setImage();
+        this.setImage(this.file);
       } else {
         this.file = e.dataTransfer.files[0];
-        this.setImage();
+        this.setImage(this.file);
       }
     },
-    setImage() {
+    setImage(file) {
       if (this.file && this.file.type.startsWith('image/')) {
-        this.clearCanvas();
-        const url = window.URL.createObjectURL(this.file);
-        const img = new window.Image();
-        img.src = url;
-        img.onload = () => {
-          const canvas = this.$refs.preview;
-          const context = canvas.getContext('2d');
-          if (img.width > canvas.width) {
-            const scale = canvas.width / img.width;
-            const sw = img.width;
-            const sh = img.height;
-            const dw = img.width * scale;
-            const dh = img.height * scale;
-            canvas.width = dw;
-            canvas.height = dh;
-            context.drawImage(img, 0, 0, sw, sh, 0, 0, dw, dh);
-          } else {
-            context.drawImage(img, 0, 0);
-          }
-        };
+        this.clearData();
+        const fr = new FileReader();
+        fr.readAsDataURL(file);
+        fr.addEventListener('load', () => {
+          this.uploadImageUrl = fr.result;
+        });
+      } else {
+        this.uploadImageUrl = '';
       }
     },
-    clearCanvas() {
-      const canvas = this.$refs.preview;
-      const context = canvas.getContext('2d');
-      context.clearRect(0, 0, canvas.width, canvas.height);
+    clearData() {
       this.results = '';
       this.translateResult = '';
     },
@@ -133,28 +129,30 @@ export default {
         console.log(error);
       }
     },
+
     getTranslateResult: async function() {
-      try {
-        const translateData = await getTranslate(this.results, this.lang);
-        if (translateData && translateData.data) {
-          this.translateResult =
-            translateData.data.translations[0].translatedText;
+      if (this.results) {
+        try {
+          const translateData = await getTranslate(this.results, this.lang);
+          if (translateData && translateData.data) {
+            this.translateResult =
+              translateData.data.translations[0].translatedText;
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    getTranslateResultNoDocs: async function() {
-      try {
-        const data = await getCloudVisionResult(this.file);
-        if (data && data.responses) {
-          const visionText = data.responses[0].fullTextAnnotation.text;
-          const translateData = await getTranslate(visionText, this.lang);
-          this.translateResult =
-            translateData.data.translations[0].translatedText;
+      } else {
+        try {
+          const data = await getCloudVisionResult(this.file);
+          if (data && data.responses) {
+            const visionText = data.responses[0].fullTextAnnotation.text;
+            const translateData = await getTranslate(visionText, this.lang);
+            this.translateResult =
+              translateData.data.translations[0].translatedText;
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
       }
     },
   },
